e9c6a9504672f6e4e0b48caaa30b7bbb0fb70704
moeen1998 <58312233+moeen1998@users.noreply.github.com>
2023-03-09 22:22:30 +0200

    init

---
+++ UserIdentityProject/UserIdentity/Data/ApplicationDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Center_ElGhalaba.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UserIdentity.Models;
+
+namespace UserIdentity.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+		public DbSet<Level> Levels { get; set; }
+		public DbSet<Stage> Stages { get; set; }
+		public DbSet<Subject> Subjects { get; set; }
+		public DbSet<LevelSubject> LevelsSubject { get; set; }
+		public DbSet<Lesson> Lessons { get; set; }
+		public DbSet<LessonResource> LessonResources { get; set; }
+		public DbSet<LessonQuiz> LessonQuizes { get; set; }
+		public DbSet<Student> Students { get; set; }
+		public DbSet<Teacher> Teachers { get; set; }
+		public DbSet<StudentOrder> StudentOrders { get; set; }
+		public DbSet<StudentHistory> StudentHistory { get; set; }
+		public DbSet<LessonComment> LessonComments { get; set; }
+		public DbSet<TeacherPaymentMethod> TeacherPaymentMethod { get; set; }
+		public DbSet<TeacherLogs> TeacherLogs { get; set; }
+		public DbSet<Follows> Follows { get; set; }
+
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>().ToTable("User", "Security");
+            builder.Entity<IdentityRole>().ToTable("Role", "Security");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaim", "Security");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaim", "Security");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRole", "Security");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogin", "Security");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserToken", "Security");
+        }
+    }
+}
---
+++ UserIdentityProject/UserIdentity/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using UserIdentity.Models;
+
+namespace Center_ElGhalaba.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        [ForeignKey ("AppUser")]
+        public string AppUserID { get; set; }
+        public virtual ApplicationUser? AppUser { get; set; }
+        public virtual List<StudentOrder>? Orders { get; set; }
+		public virtual List<LessonComment>? Comments { get; set; }
+		public virtual List<StudentHistory>? History { get; set; }
+		public virtual List<Follows>? Follows { get; set; }
+	}
+}
