da775743f5d930c6d57914c47a321a638ffda6dd
NadaMagdy777 <nadazidan607@gmail.com>
2023-03-12 08:04:52 +0200

    add studentController ,_PageLayout and  StudentDetails View

---
+++ CenterElGhlaba/UserIdentity/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using Center_ElGhalaba.Models;
+using Center_ElGhlaba.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Center_ElGhlaba.Controllers
+{
+    public class StudentController : Controller
+    {
+      
+        private readonly IUnitOfWork _unitOfWork;
+
+        public StudentController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+            {
+                return View();
+            }
+        public async Task<IActionResult> Details(int id)
+        {
+
+            return View(await _unitOfWork.Students.FindAsync(S => S.ID == id, new[] { "AppUser" }));
+        }
+    }
+}
