616f211a3a04ba18fa7597cc4ae840eb4734ca2c
NadaMagdy777 <nadazidan607@gmail.com>
2023-03-15 11:49:02 +0200

    add action GetStudentLessons and modify in details  in student controller

--- CenterElGhlaba/UserIdentity/Controllers/StudentController.cs
+++ CenterElGhlaba/UserIdentity/Controllers/StudentController.cs
@@ -15,13 +15,44 @@ namespace Center_ElGhlaba.Controllers
         }
 
         public IActionResult Index()
-            {
-                return View();
-            }
-        public async Task<IActionResult> Details(int id)
         {
+            return View();
+        }
+        public async Task<IActionResult> GetStudentLessons(int id, int pg)
+        {
+            var StudentHistory = await _unitOfWork.History.FindAllAsync(H => H.StudentID == id, new[] { "Lesson" });
 
-            return View(await _unitOfWork.Students.FindAsync(S => S.ID == id, new[] { "AppUser" }));
+            var lessons = StudentHistory.Select(L => L.Lesson).ToList();
+            const int pageSize = 1;
+
+
+
+            int recentCount = lessons.Count();
+            int recSkip = (pg - 1) * pageSize;
+            var data = lessons.Skip(recSkip).Take(pageSize).ToList();
+            return Json(data);
+
+
+        }
+
+        public async Task<IActionResult> Details(string id)
+        {
+            Student student = await _unitOfWork.Students.FindAsync(S => S.AppUser.Id == id, new[] { "AppUser" });
+
+            var StudentHistory = await _unitOfWork.History.FindAllAsync(H => H.StudentID == student.ID, new[] { "Lesson" });
+
+            List<Lesson> lessons = StudentHistory.Select(L => L.Lesson).ToList();
+            const int pageSize = 1;
+
+            int recentCount = lessons.Count();
+            Pager pager = new Pager(recentCount, 1, pageSize);
+            int recSkip = (1 - 1) * pageSize;
+            this.ViewBag.Pager = pager;
+            this.ViewBag.Lessons = lessons.Skip(recSkip).Take(pager.PageSize).ToList(); ;
+
+            return View(student);
+
+
         }
     }
 }
