0b89d0ff3348124426db0a949e27af922d3aa00e
NadaMagdy777 <nadazidan607@gmail.com>
2023-03-19 19:10:27 +0200

    add studentServices to services

---
+++ CenterElGhlaba/UserIdentity/Services/StudentServices.cs
@@ -0,0 +1,27 @@
+﻿using Center_ElGhalaba.Models;
+using Center_ElGhlaba.Interfaces;
+using Center_ElGhlaba.Unit_OfWork;
+
+namespace Center_ElGhlaba.Services
+{
+    public class StudentServices:IStudentServices
+    {
+        private readonly IUnitOfWork unit;
+
+        public StudentServices(IUnitOfWork unit)
+        {
+            this.unit = unit;
+        }
+        public async Task<Student> GetStudent(string id)
+        {
+            return await unit.Students.FindAsync(s => !s.AppUser.IsDeleted && s.AppUser.Id == id, new string[] { "AppUser" });
+
+        }
+        public async Task<List<Lesson>> GetStudentLessons(int id)
+        {
+            var orders= await unit.Orders.FindAllAsync(O => O.StudentID == id, new[] { "Lesson" }) ;
+            return orders.Select(L => L.Lesson).ToList();
+
+        }
+    }
+}
