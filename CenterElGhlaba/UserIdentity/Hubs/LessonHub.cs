2313d2eac72c25419062a9e04e46b1920dc1cf53
NadaMagdy777 <nadazidan607@gmail.com>
2023-03-24 15:49:20 +0200

    modify in index course

---
+++ CenterElGhlaba/UserIdentity/Hubs/LessonHub.cs
@@ -0,0 +1,26 @@
+﻿using Center_ElGhlaba.Interfaces;
+using Center_ElGhlaba.ViewModels;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Center_ElGhlaba.Hubs
+{
+    public class LessonHub:Hub
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public LessonHub(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public void AddLesson(LessonVM newLesson)
+        {
+
+            Clients.All.SendAsync("NewLessonAdded", newLesson);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+
+            return base.OnConnectedAsync();
+        }
+    }
+}
