90da2c03fbc6f00013782066ec0c89c2130352ed
moeen1998 <58312233+moeen1998@users.noreply.github.com>
2023-03-24 17:12:12 +0200

    final touches

--- CenterElGhlaba/UserIdentity/Controllers/TeachersController.cs
+++ CenterElGhlaba/UserIdentity/Controllers/TeachersController.cs
@@ -51,7 +51,7 @@ namespace Center_ElGhlaba.Controllers
             teacher.Follows.Add(new Follows { StudentID = student.ID, TeacherID = teacher.ID });
             unitOfWork.Complete();
 
-            await Teacherhub.Clients.Users(teacherId).SendAsync("AddFollower");
+            await Teacherhub.Clients.Users(teacherId).SendAsync("AddFollower", teacherId);
         }
         public async Task RemoveFollower(string studentId, string teacherId)
         {
@@ -63,7 +63,7 @@ namespace Center_ElGhlaba.Controllers
                 teacher.Follows.Remove(isfolow);
                 unitOfWork.Complete();
 
-                await Teacherhub.Clients.Users(teacherId).SendAsync("RemoveFollower");
+                await Teacherhub.Clients.Users(teacherId).SendAsync("RemoveFollower", teacherId);
             }
 
         }
@@ -90,7 +90,7 @@ namespace Center_ElGhlaba.Controllers
             teacher.Likes.Add(new Likes { StudentID = student.ID, TeacherID = teacher.ID });
             unitOfWork.Complete();
 
-            await Teacherhub.Clients.Users(teacherId).SendAsync("AddLike");
+            await Teacherhub.Clients.Users(teacherId).SendAsync("AddLike", teacherId);
         }
         public async Task RemoveLike(string studentId, string teacherId)
         {
@@ -102,111 +102,10 @@ namespace Center_ElGhlaba.Controllers
                 teacher.Likes.Remove(HasLike);
                 unitOfWork.Complete();
 
-                await Teacherhub.Clients.Users(teacherId).SendAsync("RemoveLike");
+                await Teacherhub.Clients.Users(teacherId).SendAsync("RemoveLike", teacherId);
             }
 
         }
 
-        //// GET: TeachersController/Details/5
-        //[Authorize(Roles = "Teacher")]
-        //public ActionResult AddCourse(int id)
-        //{
-        //    return View();
-        //}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //// GET: TeachersController
-        //[Authorize(Roles = "Admin")]
-        //public async Task<ActionResult> Index()
-        //{
-        //    return View(await unitOfWork.Teachers.GetAllAsync());
-        //}
-
-
-
-        //// GET: TeachersController/Create
-        //[Authorize(Roles ="Admin")]
-        //public ActionResult Create()
-        //{
-        //    return View();
-        //}
-
-        //// POST: TeachersController/Create
-        //[Authorize(Roles ="Admin")]
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Create(Teacher teacher)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        unitOfWork.Teachers.Insert(teacher);
-        //        unitOfWork.Complete();
-        //        return RedirectToAction("Index");
-        //    }
-
-        //    return View(teacher);
-        //}
-
-        //// GET: TeachersController/Edit/5
-        //[Authorize(Roles ="Teacher,Admin")]
-        //public async Task<ActionResult> Edit(int id)
-        //{
-        //    return View(await unitOfWork.Teachers.FindAsync(t=>t.ID == id , new[] {"AppUser"}));
-        //}
-
-        //// POST: TeachersController/Edit/5
-        //[Authorize(Roles ="Teacher,Admin")]
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<ActionResult> Edit(int id, Teacher UpdatedTeacher)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        Teacher OreginalTeacher = await unitOfWork.Teachers.FindAsync(t=>t.ID == id , new[] {"AppUser"});
-
-        //        OreginalTeacher.AppUser.FirstName = UpdatedTeacher.AppUser.FirstName;
-
-        //        //unitOfWork.Teachers.Insert(teacher);
-        //        //unitOfWork.Complete();
-        //        //return RedirectToAction("Index");
-        //    }
-
-        //    return View();
-        //}
-
-        //// GET: TeachersController/Delete/5
-        //[Authorize(Roles = "Admin")]
-        //public ActionResult Delete(int id)
-        //{
-        //    return View();
-        //}
-
-        //// POST: TeachersController/Delete/5
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Delete(int id, IFormCollection collection)
-        //{
-        //    try
-        //    {
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
     }
 }
